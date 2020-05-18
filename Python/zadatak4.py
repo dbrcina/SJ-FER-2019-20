9324c305a2c64491f01b01cb19dbee6583313ba5
dbrcina <darijo.brcina@fer.hr>
2020-05-18 11:56:56 +0200

    done

--- Python/zadatak4.py
+++ Python/zadatak4.py
@@ -1,34 +1,46 @@
 import sys
 import re
 import urllib.request
-import urllib.parse
 
 if len(sys.argv) != 2:
     print("Program očekuje jedan argument; URL web stranice.")
     sys.exit(0)
 
 web_page = urllib.request.urlopen(sys.argv[1])
-web_page_string = web_page.read().decode("utf-8")
+web_page_string = web_page.read().decode("utf8")
 print("WEB STRANICA")
 print(web_page_string)
 print("-----------------------------------")
 
-print("LINKOVI NA DRUGE STRANICE")
 links = [link.replace('"', '') for link in re.findall('href=("[^"]+")', web_page_string)]
-hosts = {}
-for link in links:
-    if "//" not in link: continue
-    print(re.search("(http.*://)([^/]+)(.*)", link).group(2))
-    host = urllib.parse.urlparse(link).netloc
-    hosts[host] = hosts.get(host, 0) + 1
+print("LINKOVI NA DRUGE STRANICE")
 print("\n".join(links))
 print("-----------------------------------")
 
-print("HOSTOVI")
+hosts = {}
+for link in links:
+    search_result = re.search("//([^/]+)/", link)
+    if search_result == None: continue
+    host = search_result.group(1)
+    counter = hosts.get(host, 0) + 1
+    hosts[host] = counter
+
 host_names = hosts.keys()
 longest_name = len(max(host_names, key=len))
+print("HOSTOVI")
 print("Host{}Broj pristupa".format(" " * longest_name))
 for host, visited in sorted(hosts.items(), key=lambda tup: tup[1], reverse=True):
     add_spaces = longest_name - len(host) + 4
     print("{}{}{}".format(host, " " * add_spaces, visited))
+print("-----------------------------------")
+
+emails = re.findall("\w+\.?\w*@\w+\.\w+", web_page_string)
+print("EMAIL ADRESE")
+if len(emails) == 0: print("Email adrese nisu pronađene")
+else: print("\n".join(emails))
+print("-----------------------------------")
+
+number_of_img_links = len(re.findall("<img[\s]src=.+", web_page_string))
+print("BROJ LINKOVA NA SLIKE")
+print(number_of_img_links)
 print("-----------------------------------")
